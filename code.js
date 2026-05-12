652cea9e6a77e9e83c0cc185eb79bdac2106cb38
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-05-12 21:07:30 +0100

    initialize project with HTML, CSS, and JavaScript files for product filtering

---
+++ code.js
@@ -0,0 +1,47 @@
+let products = [
+    { name: "T-shirt", category: "clothes", price: 20 },
+    { name: "Laptop", category: "tech", price: 999 },
+    { name: "Apple", category: "food", price: 1.50 },
+    { name: "Shoes", category: "clothes", price: 85 },
+    { name: "Phone", category: "tech", price: 699 },
+    { name: "Banana", category: "food", price: 0.50 },
+    { name: "Jacket", category: "clothes", price: 120 },
+    { name: "Bread", category: "food", price: 2.50 }
+]
+
+let prodEl = document.querySelector(".prod-container")
+let btnEl = document.getElementById("clothes-btn")
+let allEl = document.getElementById("all-btn")
+
+function renderProducts(items) {
+    prodEl.innerHTML = ""
+
+    for (let i = 0; i < items.length; i++) {
+
+        prodEl.innerHTML += `
+    
+
+<div class = "products-item">
+<p> ${products[i].name}</p> 
+<p id= "prod-${i}"> ${products[i].price}</p>
+
+</div>
+`
+    }
+}
+
+
+renderProducts(products)
+
+btnEl.addEventListener("click", function () {
+
+    let filteredProducts = products.filter(function (product) {
+        return product.category === "clothes"
+    })
+
+    renderProducts(filteredProducts)
+})
+
+allEl.addEventListener("click", function () {
+    renderProducts(products)
+})
